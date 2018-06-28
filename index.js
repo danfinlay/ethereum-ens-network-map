76d887f2478c8b7f40780338a860d767d648f1b9
Josiah Savary <hello@jsavary.com>
2018-06-28 09:19:18 -0400

    Added Rinkeby
    Using the address in this documentation: https://github.com/ensdomains/ens/blob/master/docs/introduction.rst#ens-on-ethereum

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ module.exports = {
   '1': '0x314159265dd8dbb310642f98f50c066173c1259b',
   // Ropsten
   '3': '0x112234455c3a32fd11230c42e7bccd4a84e02010',
+  // Rinkeby
+  '4': '0xe7410170f87102df0055eb195163a03b7f2bff4a',
 }
