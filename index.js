0f2918889170e2a83de9fcba714b2057d6ea2854
Thomas <thomas.b.huang@gmail.com>
2019-06-10 14:45:47 -0700

    Add Goerli

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ module.exports = {
   '1': '0x314159265dd8dbb310642f98f50c066173c1259b',
   // Ropsten
   '3': '0x112234455c3a32fd11230c42e7bccd4a84e02010',
+  // Goerli
+  '5': '0x112234455c3a32fd11230c42e7bccd4a84e02010'
 }
